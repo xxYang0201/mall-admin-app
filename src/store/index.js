27730b1312bd959c1099b6e549cab12765290ed2
jiawen.yang <958393464@qq.com>
2021-09-01 17:51:23 +0800

    home and login

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // 导航的收放状态 false为展开  true为收起来
+    collapsed: false,
+  },
+  mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
+  },
+  actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
+  },
+  modules: {
+  },
+});
