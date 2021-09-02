6fe7c749f28badd2cd2b5daed6861e051ee2b82b
jiawen.yang <958393464@qq.com>
2021-09-02 16:04:56 +0800

    router拦截

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,42 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store';
 import Home from '../views/layout/Home.vue';
 import Login from '../views/layout/Login.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: Home,
+},
+{
+  path: '/login',
+  name: 'Login',
+  component: Login,
+},
+{
+  path: '/about',
+  name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+},
 ];
 
 const router = new VueRouter({
   routes,
+});
+router.beforeEach((to, from, next) => {
+  if (to.path !== '/login') {
+    if (store.state.user.appkey && store.state.user.username) {
+      return next();
+    }
+    return next('/login');
+  }
+  return next();
 });
 
 export default router;
